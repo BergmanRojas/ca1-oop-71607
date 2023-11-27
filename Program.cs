fcc35a0bfceabb10715ab62e81d7d5fd847fa898
BergmanRojas <137055714+BergmanRojas@users.noreply.github.com>
2023-11-27 11:55:27 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Bogus;
+
+// Abstract class for parking garage
+abstract class ParkingGarage
+{
+    public string Name { get; }
+
+    protected ParkingGarage(string name)
+    {
+        Name = name;
+    }
+
+    // Abstract method to calculate charges
+    public abstract double CalculateCharges(double hoursParked);
+}
+
+
